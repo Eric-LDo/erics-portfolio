b6435d116932a2dee3cf6425c8611d825055c87d
Eric <ericld.eld@gmail.com>
2025-04-04 23:03:10 -0300

    create header estruture

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { KnowledgeComponent } from '../../components/knowledge/Knowledge.component'
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CommonModule],
+  imports: [HeaderComponent, CommonModule, KnowledgeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
