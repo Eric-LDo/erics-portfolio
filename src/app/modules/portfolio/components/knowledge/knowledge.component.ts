904773bc3410f92d03fe84dbe9ad48653c270acf
Eric <ericld.eld@gmail.com>
2025-04-06 12:04:12 -0300

    knowledge structuring

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IKnowledge } from '../../interface/IKnowledge.interfaca';
 
 @Component({
   selector: 'app-knowledge',
@@ -7,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrl: './knowledge.component.scss'
 })
 export class KnowledgeComponent {
+  public arrayKnowledge = signal<IKnowledge[]>([
+    {
+      src: '/assets/icons/knowledge/html5.svg',
+      alt: 'Ícone de conhecimento de HTML5',
+      title: 'HTML5'
+    },
+    {
+      src: '/assets/icons/knowledge/css3.svg',
+      alt: 'Ícone de conhecimento de CSS3',
+      title: 'CSS3'
 
+    },
+    {
+      src: '/assets/icons/knowledge/angular.svg',
+      alt: 'Ícone de conhecimento de AngularJs',
+      title: 'AngularJs'
+    },
+    {
+      src: '/assets/icons/knowledge/javascript.svg',
+      alt: 'Ícone de conhecimento de Javascript',
+      title: 'Javascript'
+    },
+    {
+      src: '/assets/icons/knowledge/nodejs.svg',
+      alt: 'Ícone de conhecimento de NodeJS',
+      title: 'NodeJS'
+    },
+    {
+      src: '/assets/icons/knowledge/sass.svg',
+      alt: 'Ícone de conhecimento de SASS',
+      title: 'SASS'
+    },
+    {
+      src: '/assets/icons/knowledge/react.svg',
+      alt: 'Ícone de conhecimento de Reactjs',
+      title: 'Reactjs'
+    },
+  ])
 }
