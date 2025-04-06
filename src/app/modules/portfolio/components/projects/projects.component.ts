0d7ae150b483a310dd7b14100257e1f621e91a05
Eric <ericld.eld@gmail.com>
2025-04-06 15:03:30 -0300

    projects struturing

---
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -0,0 +1,27 @@
+import { Component, signal } from '@angular/core';
+import { IProjects } from '../../interface/IProjects.interface';
+
+@Component({
+  selector: 'app-projects',
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+  public arrayProjects = signal<IProjects[]>([
+    {
+      src: '/assets/img/projects/vfull.png',
+      alt: 'Projeto Vida FullStack',
+      title: 'Vida FullStack',
+      width: '100px',
+      height: '51px',
+      description:'<p>Conheça a empresa jelpooth que procura gerar soluções inovadoras para empresas para ajudar a mante-las relevantes no mercado!</p>',
+      links: [
+        {
+          name: 'Conheça o nosso site',
+          href: 'https://jelpooth.netlify.app'
+        },
+      ]
+    }
+  ])
+}
--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgeComponent } from '../../components/knowledge/knowledge.component'
 import { CommonModule } from '@angular/common';
 import { ExperiencesComponent } from '../../components/experiences/experiences.component';
+import { ProjectsComponent } from '../../components/projects/projects.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CommonModule, KnowledgeComponent, ExperiencesComponent],
+  imports: [HeaderComponent, CommonModule, KnowledgeComponent, ExperiencesComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
