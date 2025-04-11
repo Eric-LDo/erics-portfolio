5ff20635a61ae981cd45531e07044309821399d7
Eric <ericld.eld@gmail.com>
2025-04-11 09:12:06 -0300

    add projects

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -20,14 +20,28 @@ export class ProjectsComponent {
     {
       src: '/assets/img/projects/jelpooth.png',
       alt: 'Site institucional da Jelpooth',
-      title: 'Vida FullStack',
+      title: 'Jelpooth - Site institucional ',
       width: '100px',
       height: '51px',
-      description:'<p>Conheça a empresa jelpooth que procura gerar soluções inovadoras para empresas para ajudar a mante-las relevantes no mercado!</p>',
+      description:'<p>Conheça a empresa jelpooth que procura gerar soluções inovadoras para empresas para ajudar a mante-las relevantes no mercado! (em desenvolvimento)</p>',
       links: [
         {
           name: 'Conheça o nosso site',
           href: 'https://jelpooth.netlify.app'
+        },
+      ]
+    },
+    {
+      src: '/assets/img/projects/jelpooth.png',
+      alt: 'Projeto Home Work Flow',
+      title: 'Home Work Flow',
+      width: '100px',
+      height: '51px',
+      description:'<p>Ajusda para quem tem dificuldade de aprender usando analise de dados (em desenvolvimento)</p>',
+      links: [
+        {
+          name: 'Conheça o nosso site',
+          href: 'https://homeworkflow.netlify.app'
         },
       ]
     }
